50cc3898c939a0b72da52733b5ee6a4bd96b4eab
testuserris <64862155+testuserris@users.noreply.github.com>
2020-09-17 13:32:43 +0530

    Add files via upload

---
+++ demo2 (1).tf
@@ -0,0 +1,216 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "examsdplsde" {
+  ami           = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+#here there
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+resource "aws_s3_bucket" "example" {
+
+  bucket = "terraform-getting-started-guide"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "example2" {
+
+  bucket = "mah-bucket"
+  acl    = "private"
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.lb_sg.id}"]
+  subnets            = ["${aws_subnet.public.*.id}"]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "${aws_s3_bucket.lb_logs.bucket}"
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags {
+    Environment = "production"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+}
+
+# Create a new replication instance
+resource "aws_dms_replication_instance" "test" {
+  allocated_storage            = 20
+  apply_immediately            = true
+  auto_minor_version_upgrade   = true
+  availability_zone            = "us-west-2c"
+  engine_version               = "1.9.0"
+  kms_key_arn                  = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
+  multi_az                     = false
+  preferred_maintenance_window = "sun:10:30-sun:14:30"
+  publicly_accessible          = true
+  replication_instance_class   = "dms.t2.micro"
+  replication_instance_id      = "test-dms-replication-instance-tf"
+  replication_subnet_group_id  = "${aws_dms_replication_subnet_group.test-dms-replication-subnet-group-tf}"
+
+  tags {
+    Name = "test"
+  }
+
+  vpc_security_group_ids = [
+    "sg-12345678",
+  ]
+}
+
+# Create a new load balancer
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  access_logs {
+    bucket        = "foo"
+    bucket_prefix = "bar"
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 8000
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.foo.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "foobar-terraform-elb"
+  }
+}
+
+# aws launch config
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name          = "web_config"
+  image_id      = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+}
+# end aws l c
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 2
+  identifier         = "aurora-cluster-demo-${count.index}"
+  cluster_identifier = "${aws_rds_cluster.default.id}"
+  instance_class     = "db.r3.large"
+}
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier = "aurora-cluster-demo"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name      = "mydb"
+  master_username    = "foo"
+  master_password    = "barbut8chars"
+}
+
+
+resource "aws_redshift_cluster" "default" {
+  cluster_identifier = "tf-redshift-cluster"
+  database_name      = "mydb"
+  master_username    = "foo"
+  master_password    = "Mustbe8characters"
+  node_type          = "dc1.large"
+  cluster_type       = "single-node"
+}
+
+resource "aws_iam_account_password_policy" "strkjihugyftyijhugyhuugyvhbjicsgyftrcgvhbgyftgvhghbst" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = false
+  require_uppercase_characters   = false
+  require_symbols                = false
+  allow_users_to_change_password = true
+}
+
+resource "aws_iam_account_password_policy" "strict_2" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = false
+  require_numbers                = true
+  require_uppercase_characters   = false
+  require_symbols                = false
+  allow_users_to_change_password = true
+}
+
+resource "aws_cloudtrail" "examplelmjihugyftdyuhijokpoiugyftyguhijoi" {
+
+  is_multi_region_trail = true
+
+  event_selector {
+    read_write_type = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
+}
+
