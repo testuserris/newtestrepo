9a0cc90543820d042c23f64877b77d963218fe38
testuserris <64862155+testuserris@users.noreply.github.com>
2023-11-01 19:17:48 +0530

    Update testterratoday.tf

--- testterratoday.tf
+++ testterratoday.tf
@@ -7,15 +7,6 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-
-  }
 
   filter {
     name   = "virtualization-type"
